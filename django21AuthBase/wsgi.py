712bad50e3dec6dc5431ea1d6b777ec1b16bdba8
Seiji Osada <seiji13r@live.com>
2018-09-02 00:38:11 -0500

    My initial local commit

---
+++ django21AuthBase/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for django21AuthBase project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django21AuthBase.settings')
+
+application = get_wsgi_application()
